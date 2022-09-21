5ddd3370d0adfcdb11d0228fdd5be8f2ec67949f
Evandro Pomatti <evandro.pomatti@gmail.com>
2022-09-21 19:26:31 -0300

    code response type

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -2,41 +2,37 @@ import * as dotenv from 'dotenv';
 dotenv.config();
 import express from 'express';
 import cors from 'cors';
-import * as oidc from 'express-openid-connect';
+import { auth, requiresAuth, ConfigParams } from 'express-openid-connect';
 
 const app = express();
 app.use(express.json());
 app.use(cors())
-app.use(
-  oidc.auth({
-    issuerBaseURL: process.env.ISSUER_BASE_URL,
-    baseURL: process.env.BASE_URL,
-    clientID: process.env.CLIENT_ID,
-    secret: process.env.SECRET,
-    idpLogout: true,
-  })
-);
+
+const config: ConfigParams = {
+  authRequired: true,
+  baseURL: process.env.BASE_URL,
+  clientID: process.env.CLIENT_ID,
+  issuerBaseURL: process.env.ISSUER_BASE_URL,
+  clientSecret: process.env.SECRET,
+  authorizationParams: {
+    response_type: "code"
+  }
+};
+
+app.use(auth(config));
 
 export const start = async () => {
 
-  app.get('/api/data', (req, res) => {
-    const data = {
-      bankBalance: 50,
-      aaa: 3
-    }
-    res.status(200).send(data);
-  })
+  app.get('/', (req, res) => {
+    res.send(req.oidc.isAuthenticated() ? 'Logged in' : 'Logged out')
+  });
 
-  app.get('/profile', (req: Express.Request, res: Express.Response) => {
-    const user = req.oidc.user;
-    console.log(user);
-  })
+  app.get('/profile', requiresAuth(), (req, res) => {
+    res.send(JSON.stringify(req.oidc.user));
+  });
 
   const port = 5000;
   app.listen(port, () => {
-    console.log(`Worker endpoint started on port ${port}`)
+    console.log(`Server started on port ${port}`)
   })
 }
-
-
-const { auth } = require('express-openid-connect');

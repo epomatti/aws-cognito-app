9f26e7dab93110b43580a4930a8316958a6c4ec1
Evandro Pomatti <evandro.pomatti@gmail.com>
2023-06-05 00:14:13 -0300

    output

--- infra/output.tf
+++ infra/output.tf
@@ -1,12 +1,12 @@
-# output "cognito_oidc_issuer_endpoint_url" {
-#   value       = "https://${aws_cognito_user_pool.main.endpoint}"
-#   description = "This is the OIDC Issuer endpoint URL"
-# }
+output "cognito_oidc_issuer_endpoint_url" {
+  value       = "https://${aws_cognito_user_pool.main.endpoint}"
+  description = "This is the OIDC Issuer endpoint URL"
+}
 
-# output "cognito_client_id" {
-#   value = aws_cognito_user_pool_client.main.id
-# }
+output "cognito_client_id" {
+  value = aws_cognito_user_pool_client.main.id
+}
 
-# output "cognito_get_client_secret_command" {
-#   value = "aws cognito-idp describe-user-pool-client --user-pool-id ${aws_cognito_user_pool.main.id} --client-id ${aws_cognito_user_pool_client.main.id}"
-# }
+output "cognito_get_client_secret_command" {
+  value = "aws cognito-idp describe-user-pool-client --user-pool-id ${aws_cognito_user_pool.main.id} --client-id ${aws_cognito_user_pool_client.main.id}"
+}
